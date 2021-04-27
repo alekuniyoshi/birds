319bae89053ff4973800e81c8d7ba84653b7790e
Alejandro <alejandro.kuniyoshi@gmail.com>
2021-04-26 21:38:44 -0300

    pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { IndexComponent } from './pages/index/index.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PhotoDetailComponent } from './pages/photo-detail/photo-detail.component';
+import { VideosComponent } from './pages/videos/videos.component';
+import { VideosDetailComponent } from './pages/videos-detail/videos-detail.component';
 
 
 @NgModule({
@@ -12,6 +18,12 @@ import { FooterComponent } from './shared/footer/footer.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    IndexComponent,
+    AboutComponent,
+    ContactComponent,
+    PhotoDetailComponent,
+    VideosComponent,
+    VideosDetailComponent,
 
   ],
   imports: [
---
+++ src/app/pages/videos-detail/videos-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-videos-detail',
+  templateUrl: './videos-detail.component.html',
+  styleUrls: ['./videos-detail.component.css']
+})
+export class VideosDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
